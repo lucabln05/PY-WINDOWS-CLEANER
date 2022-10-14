44c469d13c66997debad9d724373d1b906a1739e
lucabln05 <luca.bellon@icloud.com>
2022-10-14 13:48:12 +0200

    Version 1.0

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+
+
+
 
 print("""
 Willkommen im Windows Cache CLEANER
@@ -7,14 +11,41 @@ Version 1.0
 Nutzung auf eigene Gefahr, keine Haftung fuer Schaeden oder Datenverlust.
 """)
 
+
 def user_start_question():
     user_input_start = input("Willst du deinen Windows Cache bereinigen?(y/n):  ")
 
     if user_input_start == "y":
-        print("START")
+        cache_clean_process()
     elif user_input_start == "n":
         print("Vorgang wurde abgebrochen")
         user_start_question()
     else:
         print("Befehl leider nicht verstanden")
         user_start_question()
+
+def cache_clean_process():
+    print("Process start")
+    temp_file_location = os.getenv('temp')
+    print(temp_file_location)
+    os.chmod(temp_file_location, 0o777)
+    dir = temp_file_location
+
+
+    for file in os.listdir(dir):
+        try:
+            os.remove(os.path.join(dir, file))
+        except PermissionError as err:
+            print(f"{file} konnte nicht entfernt werden da: {err}" )
+
+
+
+
+
+
+
+
+
+
+
+user_start_question()
