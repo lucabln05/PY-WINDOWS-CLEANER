c2063bece9291c68efd97e3197c435e4f3c0abc4
lucabln05 <luca.bellon@icloud.com>
2022-10-14 14:52:19 +0200

    Version 1.0

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 import shutil
 #https://linuxize.com/post/python-delete-files-and-directories/
 
+import winshell         #must also install pypiwin32 (pip install winshell and pip install pypwin32)
+
+#https://github.com/tjguk/winshell
+
+
 print("""
 Willkommen im Windows Cache CLEANER
 
@@ -15,7 +20,7 @@ def user_start_question():
     user_input_start = input("Willst du deinen Windows Cache bereinigen?(y/n):  ")
 
     if user_input_start == "y":
-        cache_clean_process()
+        clean_process()
     elif user_input_start == "n":
         print("Vorgang wurde abgebrochen")
         user_start_question()
@@ -23,34 +28,72 @@ def user_start_question():
         print("Befehl leider nicht verstanden")
         user_start_question()
 
-#TEMP Ordner wird Lokalisiert und alle Dateien darin geloescht
 
-def cache_clean_process():
+#Delete file to make more space and speedup windows
+def clean_process():
     print("Process startet ...")
 
-    temp_file_location = os.getenv('temp')  #Temp Ordner wir mit hilfe os.gentev gesucht
+    # TEMP Ordner wird Lokalisiert und alle Dateien darin geloescht
+    #
+    def delete_temp_files():
+        temp_file_location = os.getenv('temp')  #Temp Ordner wir mit hilfe os.gentev gesucht
 
-    # Rechte werden angepasst das Python Script die meisten Dateien Loeschen kann
-    try:
-        os.chmod(temp_file_location, 0o777)
-    except OSError as err:
-        print(f"Fehler beim anpassen der Rechte: {err}")
-
-
-    #Inhalt aus temp ordner wird ausgelesen, wenn ordner dann wir mit shutil.rmtree dieser entfernt.
-    for dir_ in os.scandir(temp_file_location):
-        if dir_.is_dir():
-            try:
-                shutil.rmtree(dir_.path)
-            except OSError as err:
-                print(f"{dir_.path} konnte nicht entfernt werden da: {err}")
-
-    # Inhalt aus temp ordner werden ausgelese, wenn datei dann wird mit os.remove datei geloescht.
-    for file in os.listdir(temp_file_location):
+        # Rechte werden angepasst das Python Script die meisten Dateien Loeschen kann
         try:
-            os.remove(os.path.join(temp_file_location, file))
-        except PermissionError as err:
-            print(f"{file} konnte nicht entfernt werden da: {err}")
+            os.chmod(temp_file_location, 0o777)
+        except OSError as err:
+            print(f"Fehler beim anpassen der Rechte: {err}")
+
+
+        #Inhalt aus temp ordner wird ausgelesen, wenn ordner dann wir mit shutil.rmtree dieser entfernt.
+        for dir_ in os.scandir(temp_file_location):
+            if dir_.is_dir():
+                try:
+                    shutil.rmtree(dir_.path)
+                except OSError as err:
+                    print(f"{dir_.path} konnte nicht entfernt werden da: {err}")
+
+        # Inhalt aus temp ordner werden ausgelese, wenn datei dann wird mit os.remove datei geloescht.
+        for file in os.listdir(temp_file_location):
+            try:
+                os.remove(os.path.join(temp_file_location, file))
+            except PermissionError as err:
+                print(f"{file} konnte nicht entfernt werden da: {err}")
+
+    # Papierkorb wird gelert mittels winshell
+    #
+    def delete_trash_bin():
+        try:
+            winshell.recycle_bin().empty(confirm=False, show_progress=False, sound=False)
+        except Exception as err:
+            print(err)
+
+    # WINDOWS.OLD Folder wird gelert mittels winshell
+    #
+    def delete_windows_old():
+        temp_file_location = '/Windows.old'
+
+        # Rechte werden angepasst das Python Script die meisten Dateien Loeschen kann
+        try:
+            os.chmod(temp_file_location, 0o777)
+        except OSError as err:
+            print(f"Fehler beim anpassen der Rechte: {err}")
+
+        # Inhalt aus temp ordner wird ausgelesen, wenn ordner dann wir mit shutil.rmtree dieser entfernt.
+        try:
+            shutil.rmtree(temp_file_location)
+        except OSError as err:
+            print(f"{temp_file_location} konnte nicht entfernt werden da: {err}")
+
+
+    delete_temp_files()
+    delete_trash_bin()
+    delete_windows_old()
+
+input("Process abgeschlossen")
+
+
+
 
 
 
